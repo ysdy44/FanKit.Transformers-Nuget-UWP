6f2b3af2258ff72b50cfa025c9dc9f800ade412c
ysdy44 <ysdy44@outlook.com>
2019-06-21 17:41:52 +0800

    添加项目文件。

---
+++ FanKit.Transformers/Icon.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace FanKit.Transformers
+{
+    /// <summary>
+    /// Icon of <see cref = "Transformer" />. 
+    /// </summary>
+    public sealed partial class Icon : UserControl
+    {       
+        //@Converter
+        public Icon()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
