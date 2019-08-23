c79e55fc9d4ca3e3bc8b308272778fe468ff7ae1
ysdy44 <ysdy44@outlook.com>
2019-08-23 15:42:14 +0800

    Add CanvasDrawingSessionExtensions.Transformer.cs file.

---
+++ FanKit.Transformers/CanvasDrawingSessionExtensions.Transformer.cs
@@ -0,0 +1,145 @@
+﻿using Microsoft.Graphics.Canvas;
+using System.Numerics;
+
+namespace FanKit.Transformers
+{
+    /// <summary>
+    /// Extensions of <see cref = "CanvasDrawingSession" />.
+    /// </summary>
+    public static partial class CanvasDrawingSessionExtensions
+    {
+
+        private static void _drawBound(CanvasDrawingSession drawingSession, Vector2 leftTop, Vector2 rightTop, Vector2 rightBottom, Vector2 leftBottom, Windows.UI.Color accentColor)
+        {
+            drawingSession.DrawLine(leftTop, rightTop, accentColor);
+            drawingSession.DrawLine(rightTop, rightBottom, accentColor);
+            drawingSession.DrawLine(rightBottom, leftBottom, accentColor);
+            drawingSession.DrawLine(leftBottom, leftTop, accentColor);
+        }
+
+        /// <summary>
+        /// Draw lines on bound.
+        /// </summary>
+        /// <param name="drawingSession"> The drawing-session. </param>
+        /// <param name="transformer"> The transformer. </param>
+        public static void DrawBound(this CanvasDrawingSession drawingSession, Transformer transformer) => CanvasDrawingSessionExtensions._drawBound(drawingSession, transformer.LeftTop, transformer.RightTop, transformer.RightBottom, transformer.LeftBottom, Windows.UI.Colors.DodgerBlue);
+
+        /// <summary>
+        /// Draw lines on bound.
+        /// </summary>
+        /// <param name="drawingSession"> The drawing-session. </param>
+        /// <param name="transformer"> The transformer. </param>
+        /// <param name="matrix"> The matrix. </param>
+        public static void DrawBound(this CanvasDrawingSession drawingSession, Transformer transformer, Matrix3x2 matrix) => CanvasDrawingSessionExtensions._drawBound(drawingSession, Vector2.Transform(transformer.LeftTop, matrix), Vector2.Transform(transformer.RightTop, matrix), Vector2.Transform(transformer.RightBottom, matrix), Vector2.Transform(transformer.LeftBottom, matrix), Windows.UI.Colors.DodgerBlue);
+
+
+        /// <summary>
+        /// Draw lines on bound.
+        /// </summary>
+        /// <param name="drawingSession"> The drawing-session. </param>
+        /// <param name="transformer"> transformer </param>
+        /// <param name="accentColor"> The accent color. </param>
+        public static void DrawBound(this CanvasDrawingSession drawingSession, Transformer transformer, Windows.UI.Color accentColor) => CanvasDrawingSessionExtensions._drawBound(drawingSession, transformer.LeftTop, transformer.RightTop, transformer.RightBottom, transformer.LeftBottom, accentColor);
+
+        /// <summary>
+        /// Draw lines on bound.
+        /// </summary>
+        /// <param name="drawingSession"> The drawing-session. </param>
+        /// <param name="transformer"> The transformer. </param>
+        /// <param name="matrix"> The matrix. </param>
+        /// <param name="accentColor"> The accent color. </param>
+        public static void DrawBound(this CanvasDrawingSession drawingSession, Transformer transformer, Matrix3x2 matrix, Windows.UI.Color accentColor) => CanvasDrawingSessionExtensions._drawBound(drawingSession, Vector2.Transform(transformer.LeftTop, matrix), Vector2.Transform(transformer.RightTop, matrix), Vector2.Transform(transformer.RightBottom, matrix), Vector2.Transform(transformer.LeftBottom, matrix), accentColor);
+
+
+
+        private static void _drawBoundNodes(CanvasDrawingSession drawingSession, Vector2 leftTop, Vector2 rightTop, Vector2 rightBottom, Vector2 leftBottom, Windows.UI.Color accentColor, bool disabledRadian)
+        {
+            //Line
+            drawingSession.DrawThickLine(leftTop, rightTop);
+            drawingSession.DrawThickLine(rightTop, rightBottom);
+            drawingSession.DrawThickLine(rightBottom, leftBottom);
+            drawingSession.DrawThickLine(leftBottom, leftTop);
+
+            //Center
+            Vector2 centerLeft = (leftTop + leftBottom) / 2;
+            Vector2 centerTop = (leftTop + rightTop) / 2;
+            Vector2 centerRight = (rightTop + rightBottom) / 2;
+            Vector2 centerBottom = (leftBottom + rightBottom) / 2;
+
+            //Scale2
+            drawingSession.DrawNode2(leftTop);
+            drawingSession.DrawNode2(rightTop);
+            drawingSession.DrawNode2(rightBottom);
+            drawingSession.DrawNode2(leftBottom);
+
+            if (disabledRadian == false)
+            {
+                //Outside
+                Vector2 outsideLeft = Transformer._outsideNode(centerLeft, centerRight);
+                Vector2 outsideTop = Transformer._outsideNode(centerTop, centerBottom);
+                Vector2 outsideRight = Transformer._outsideNode(centerRight, centerLeft);
+                Vector2 outsideBottom = Transformer._outsideNode(centerBottom, centerTop);
+
+                //Radian
+                drawingSession.DrawThickLine(outsideTop, centerTop);
+                drawingSession.DrawNode(outsideTop);
+
+                //Skew
+                //drawingSession.DrawNode2(drawingSession, outsideTop);
+                //drawingSession.DrawNode2(drawingSession, outsideLeft);
+                drawingSession.DrawNode2(outsideRight);
+                drawingSession.DrawNode2(outsideBottom);
+            }
+
+            //Scale1
+            if (Transformer.OutNodeDistance(centerLeft, centerRight))
+            {
+                drawingSession.DrawNode2(centerTop);
+                drawingSession.DrawNode2(centerBottom);
+            }
+            if (Transformer.OutNodeDistance(centerTop, centerBottom))
+            {
+                drawingSession.DrawNode2(centerLeft);
+                drawingSession.DrawNode2(centerRight);
+            }
+        }
+
+        /// <summary>
+        /// Draw nodes and lines on bound，just like【由】.
+        /// </summary>
+        /// <param name="drawingSession"> The drawing-session. </param>
+        /// <param name="transformer"> The transformer. </param>
+        /// <param name="disabledRadian"> Disable the rotation angle. </param>
+        public static void DrawBoundNodes(this CanvasDrawingSession drawingSession, Transformer transformer, bool disabledRadian = false) => CanvasDrawingSessionExtensions._drawBoundNodes(drawingSession, transformer.LeftTop, transformer.RightTop, transformer.RightBottom, transformer.LeftBottom, Windows.UI.Colors.DodgerBlue, disabledRadian);
+
+        /// <summary>
+        /// Draw nodes and lines on bound，just like【由】.
+        /// </summary>
+        /// <param name="drawingSession"> The drawing-session. </param>
+        /// <param name="transformer"> The transformer. </param>
+        /// <param name="matrix"> The matrix. </param>
+        /// <param name="disabledRadian"> Disable the rotation angle. </param>
+        public static void DrawBoundNodes(this CanvasDrawingSession drawingSession, Transformer transformer, Matrix3x2 matrix, bool disabledRadian = false) => CanvasDrawingSessionExtensions._drawBoundNodes(drawingSession, Vector2.Transform(transformer.LeftTop, matrix), Vector2.Transform(transformer.RightTop, matrix), Vector2.Transform(transformer.RightBottom, matrix), Vector2.Transform(transformer.LeftBottom, matrix), Windows.UI.Colors.DodgerBlue, disabledRadian);
+
+
+        /// <summary>
+        /// Draw nodes and lines on bound，just like【由】.
+        /// </summary>
+        /// <param name="drawingSession"> The drawing-session. </param>
+        /// <param name="transformer"> The transformer. </param>
+        /// <param name="accentColor"> The accent color. </param>
+        /// <param name="disabledRadian"> Disable the rotation angle. </param>
+        public static void DrawBoundNodes(this CanvasDrawingSession drawingSession, Transformer transformer, Windows.UI.Color accentColor, bool disabledRadian = false) => CanvasDrawingSessionExtensions._drawBoundNodes(drawingSession, transformer.LeftTop, transformer.RightTop, transformer.RightBottom, transformer.LeftBottom, accentColor, disabledRadian);
+
+        /// <summary>
+        /// Draw nodes and lines on bound，just like【由】.
+        /// </summary>
+        /// <param name="drawingSession"> The drawing-session. </param>
+        /// <param name="transformer"> The transformer. </param>
+        /// <param name="matrix"> The matrix. </param>
+        /// <param name="accentColor"> The accent color. </param>
+        /// <param name="disabledRadian"> Disable the rotation angle. </param>
+        public static void DrawBoundNodes(this CanvasDrawingSession drawingSession, Transformer transformer, Matrix3x2 matrix, Windows.UI.Color accentColor, bool disabledRadian = false) => CanvasDrawingSessionExtensions._drawBoundNodes(drawingSession, Vector2.Transform(transformer.LeftTop, matrix), Vector2.Transform(transformer.RightTop, matrix), Vector2.Transform(transformer.RightBottom, matrix), Vector2.Transform(transformer.LeftBottom, matrix), accentColor, disabledRadian);
+
+    }
+}
--- FanKit.Transformers/CanvasDrawingSessionExtensions.cs
+++ FanKit.Transformers/CanvasDrawingSessionExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.Geometry;
 using System.Numerics;
 
 namespace FanKit.Transformers
@@ -16,7 +17,7 @@ namespace FanKit.Transformers
         /// <param name="point0"> The frist point. </param>
         /// <param name="point1"> The second point. </param>
         public static void DrawLineDodgerBlue(this CanvasDrawingSession drawingSession, Vector2 point0, Vector2 point1) => drawingSession.DrawLine(point0, point1, Windows.UI.Colors.DodgerBlue);
-     
+
         /// <summary>
         /// Draw a ——.
         /// </summary>
@@ -28,140 +29,43 @@ namespace FanKit.Transformers
             drawingSession.DrawLine(point0, point1, Windows.UI.Colors.Black, 2);
             drawingSession.DrawLine(point0, point1, Windows.UI.Colors.White);
         }
-        
 
 
-        private static void _drawBound(CanvasDrawingSession drawingSession, Vector2 leftTop, Vector2 rightTop, Vector2 rightBottom, Vector2 leftBottom, Windows.UI.Color accentColor)
+
+        private static void _fillRectDodgerBlue(CanvasDrawingSession drawingSession, ICanvasResourceCreator resourceCreator, Vector2 leftTop, Vector2 rightTop, Vector2 rightBottom, Vector2 leftBottom)
         {
-            drawingSession.DrawLine(leftTop, rightTop, accentColor);
-            drawingSession.DrawLine(rightTop, rightBottom, accentColor);
-            drawingSession.DrawLine(rightBottom, leftBottom, accentColor);
-            drawingSession.DrawLine(leftBottom, leftTop, accentColor);
+            //Points
+            Vector2[] points = new Vector2[]
+            {
+                leftTop,
+                rightTop,
+                rightBottom,
+                leftBottom
+            };
+
+            //Geometry
+            CanvasGeometry canvasGeometry = CanvasGeometry.CreatePolygon(resourceCreator, points);
+            drawingSession.FillGeometry(canvasGeometry, Windows.UI.Color.FromArgb(90, 54, 135, 230));
+            drawingSession.DrawGeometry(canvasGeometry, Windows.UI.Colors.DodgerBlue);
         }
 
-        /// <summary>
-        /// Draw lines on bound.
-        /// </summary>
-        /// <param name="drawingSession"> The drawing-session. </param>
-        /// <param name="transformer"> The transformer. </param>
-        public static void DrawBound(this CanvasDrawingSession drawingSession, Transformer transformer) => CanvasDrawingSessionExtensions._drawBound(drawingSession, transformer.LeftTop, transformer.RightTop, transformer.RightBottom, transformer.LeftBottom, Windows.UI.Colors.DodgerBlue);
 
         /// <summary>
-        /// Draw lines on bound.
+        /// Fill a Rectangular (color is <see cref="Windows.UI.Colors.DodgerBlue"/>).
         /// </summary>
         /// <param name="drawingSession"> The drawing-session. </param>
-        /// <param name="transformer"> The transformer. </param>
+        /// <param name="resourceCreator"> The resource-creator. </param>
+        /// <param name="transformerRect"> The transformer-rect. </param>
+        public static void FillRectDodgerBlue(this CanvasDrawingSession drawingSession, ICanvasResourceCreator resourceCreator, TransformerRect transformerRect) => CanvasDrawingSessionExtensions._fillRectDodgerBlue(drawingSession, resourceCreator, transformerRect.LeftTop, transformerRect.RightTop, transformerRect.RightBottom, transformerRect.LeftBottom);
+
+        /// <summary>
+        /// Fill a Rectangular (color is <see cref="Windows.UI.Colors.DodgerBlue"/>).
+        /// </summary>
+        /// <param name="drawingSession"> The drawing-session. </param>
+        /// <param name="resourceCreator"> The resource-creator. </param>
+        /// <param name="transformerRect"> The transformer-rect. </param>
         /// <param name="matrix"> The matrix. </param>
-        public static void DrawBound(this CanvasDrawingSession drawingSession, Transformer transformer, Matrix3x2 matrix) => CanvasDrawingSessionExtensions._drawBound(drawingSession, Vector2.Transform(transformer.LeftTop, matrix), Vector2.Transform(transformer.RightTop, matrix), Vector2.Transform(transformer.RightBottom, matrix), Vector2.Transform(transformer.LeftBottom, matrix), Windows.UI.Colors.DodgerBlue);
-     
-        
-        /// <summary>
-        /// Draw lines on bound.
-        /// </summary>
-        /// <param name="drawingSession"> The drawing-session. </param>
-        /// <param name="transformer"> transformer </param>
-        /// <param name="accentColor"> The accent color. </param>
-        public static void DrawBound(this CanvasDrawingSession drawingSession, Transformer transformer,  Windows.UI.Color accentColor) => CanvasDrawingSessionExtensions._drawBound(drawingSession, transformer.LeftTop, transformer.RightTop, transformer.RightBottom, transformer.LeftBottom, accentColor);
+        public static void FillRectDodgerBlue(this CanvasDrawingSession drawingSession, ICanvasResourceCreator resourceCreator, TransformerRect transformerRect, Matrix3x2 matrix) => CanvasDrawingSessionExtensions._fillRectDodgerBlue(drawingSession, resourceCreator, Vector2.Transform(transformerRect.LeftTop, matrix), Vector2.Transform(transformerRect.RightTop, matrix), Vector2.Transform(transformerRect.RightBottom, matrix), Vector2.Transform(transformerRect.LeftBottom, matrix));
 
-        /// <summary>
-        /// Draw lines on bound.
-        /// </summary>
-        /// <param name="drawingSession"> The drawing-session. </param>
-        /// <param name="transformer"> The transformer. </param>
-        /// <param name="matrix"> The matrix. </param>
-        /// <param name="accentColor"> The accent color. </param>
-        public static void DrawBound(this CanvasDrawingSession drawingSession, Transformer transformer, Matrix3x2 matrix, Windows.UI.Color accentColor) => CanvasDrawingSessionExtensions._drawBound(drawingSession, Vector2.Transform(transformer.LeftTop, matrix), Vector2.Transform(transformer.RightTop, matrix), Vector2.Transform(transformer.RightBottom, matrix), Vector2.Transform(transformer.LeftBottom, matrix),accentColor);
-
-
-
-
-        private static void _drawBoundNodes(CanvasDrawingSession drawingSession, Vector2 leftTop, Vector2 rightTop, Vector2 rightBottom, Vector2 leftBottom, Windows.UI.Color accentColor, bool disabledRadian )
-        {
-            //Line
-            drawingSession.DrawThickLine(leftTop, rightTop);
-            drawingSession.DrawThickLine(rightTop, rightBottom);
-            drawingSession.DrawThickLine(rightBottom, leftBottom);
-            drawingSession.DrawThickLine(leftBottom, leftTop);
-
-            //Center
-            Vector2 centerLeft = (leftTop + leftBottom) / 2;
-            Vector2 centerTop = (leftTop + rightTop) / 2;
-            Vector2 centerRight = (rightTop + rightBottom) / 2;
-            Vector2 centerBottom = (leftBottom + rightBottom) / 2;
-
-            //Scale2
-            drawingSession.DrawNode2(leftTop);
-            drawingSession.DrawNode2(rightTop);
-            drawingSession.DrawNode2(rightBottom);
-            drawingSession.DrawNode2(leftBottom);
-
-            if (disabledRadian == false)
-            {
-                //Outside
-                Vector2 outsideLeft = Transformer._outsideNode(centerLeft, centerRight);
-                Vector2 outsideTop = Transformer._outsideNode(centerTop, centerBottom);
-                Vector2 outsideRight = Transformer._outsideNode(centerRight, centerLeft);
-                Vector2 outsideBottom = Transformer._outsideNode(centerBottom, centerTop);
-
-                //Radian
-                drawingSession.DrawThickLine(outsideTop, centerTop);
-                drawingSession.DrawNode(outsideTop);
-
-                //Skew
-                //drawingSession.DrawNode2(drawingSession, outsideTop);
-                //drawingSession.DrawNode2(drawingSession, outsideLeft);
-                drawingSession.DrawNode2(outsideRight);
-                drawingSession.DrawNode2(outsideBottom);
-            }
-
-            //Scale1
-            if (Transformer.OutNodeDistance(centerLeft, centerRight))
-            {
-                drawingSession.DrawNode2(centerTop);
-                drawingSession.DrawNode2(centerBottom);
-            }
-            if (Transformer.OutNodeDistance(centerTop, centerBottom))
-            {
-                drawingSession.DrawNode2(centerLeft);
-                drawingSession.DrawNode2(centerRight);
-            }
-        }
-
-        /// <summary>
-        /// Draw nodes and lines on bound，just like【由】.
-        /// </summary>
-        /// <param name="drawingSession"> The drawing-session. </param>
-        /// <param name="transformer"> The transformer. </param>
-        /// <param name="disabledRadian"> Disable the rotation angle. </param>
-        public static void DrawBoundNodes(this CanvasDrawingSession drawingSession, Transformer transformer,  bool disabledRadian = false) => CanvasDrawingSessionExtensions._drawBoundNodes(drawingSession, transformer.LeftTop,  transformer.RightTop,  transformer.RightBottom,  transformer.LeftBottom,  Windows.UI.Colors.DodgerBlue, disabledRadian);
-
-        /// <summary>
-        /// Draw nodes and lines on bound，just like【由】.
-        /// </summary>
-        /// <param name="drawingSession"> The drawing-session. </param>
-        /// <param name="transformer"> The transformer. </param>
-        /// <param name="matrix"> The matrix. </param>
-        /// <param name="disabledRadian"> Disable the rotation angle. </param>
-        public static void DrawBoundNodes(this CanvasDrawingSession drawingSession, Transformer transformer, Matrix3x2 matrix, bool disabledRadian = false) => CanvasDrawingSessionExtensions._drawBoundNodes(drawingSession, Vector2.Transform(transformer.LeftTop, matrix), Vector2.Transform(transformer.RightTop, matrix), Vector2.Transform(transformer.RightBottom, matrix), Vector2.Transform(transformer.LeftBottom, matrix), Windows.UI.Colors.DodgerBlue, disabledRadian);
-
-
-        /// <summary>
-        /// Draw nodes and lines on bound，just like【由】.
-        /// </summary>
-        /// <param name="drawingSession"> The drawing-session. </param>
-        /// <param name="transformer"> The transformer. </param>
-        /// <param name="accentColor"> The accent color. </param>
-        /// <param name="disabledRadian"> Disable the rotation angle. </param>
-        public static void DrawBoundNodes(this CanvasDrawingSession drawingSession, Transformer transformer,  Windows.UI.Color accentColor, bool disabledRadian = false) => CanvasDrawingSessionExtensions._drawBoundNodes(drawingSession, transformer.LeftTop,  transformer.RightTop,  transformer.RightBottom,  transformer.LeftBottom,  accentColor, disabledRadian);
-
-        /// <summary>
-        /// Draw nodes and lines on bound，just like【由】.
-        /// </summary>
-        /// <param name="drawingSession"> The drawing-session. </param>
-        /// <param name="transformer"> The transformer. </param>
-        /// <param name="matrix"> The matrix. </param>
-        /// <param name="accentColor"> The accent color. </param>
-        /// <param name="disabledRadian"> Disable the rotation angle. </param>
-        public static void DrawBoundNodes(this CanvasDrawingSession drawingSession, Transformer transformer, Matrix3x2 matrix, Windows.UI.Color accentColor, bool disabledRadian = false) => CanvasDrawingSessionExtensions._drawBoundNodes(drawingSession, Vector2.Transform(transformer.LeftTop, matrix), Vector2.Transform(transformer.RightTop, matrix), Vector2.Transform(transformer.RightBottom, matrix), Vector2.Transform(transformer.LeftBottom, matrix), accentColor, disabledRadian);
     }
 }
