29b2451da4414afa9605506e35733d3fde14fe7e
ysdy44 <ysdy44@outlook.com>
2024-02-10 21:36:47 +0800

    Add Transformer.FindHomography3D()

--- FanKit.Transformers/Transformer.Homography.cs
+++ FanKit.Transformers/Transformer.Homography.cs
@@ -15,6 +15,18 @@ namespace FanKit.Transformers
             M32 = transformer.LeftTop.Y,
         };
 
+        internal static Matrix4x4 FindHomographyFromIdentity3D(ITransformerLTRB transformer) => new Matrix4x4
+        {
+            M11 = transformer.RightTop.X - transformer.LeftTop.X,
+            M12 = transformer.RightTop.Y - transformer.LeftTop.Y,
+            M21 = transformer.LeftBottom.X - transformer.LeftTop.X,
+            M22 = transformer.LeftBottom.Y - transformer.LeftTop.Y,
+            M33 = 1,
+            M41 = transformer.LeftTop.X,
+            M42 = transformer.LeftTop.Y,
+            M44 = 1,
+        };
+
         internal static Matrix3x2 FindHomographyFromIdentity(float x, float y, float width, float height) => new Matrix3x2(
                 width, // ScaleX
                 0, // SkewY
@@ -36,6 +48,19 @@ namespace FanKit.Transformers
         /// <summary>
         /// Find Homography.  
         /// </summary>
+        /// <param name="sourceWidth"> The width of source rectangle. </param>
+        /// <param name="sourceHeight"> The height of source rectangle. </param>
+        /// <param name="destination"> The destination Transformer. </param>
+        /// <returns> The homologous matrix. </returns>
+        public static Matrix4x4 FindHomography3D(float sourceWidth, float sourceHeight, ITransformerLTRB destination)
+        {
+            Matrix4x4 S = FindHomographyFromIdentity3D(destination);
+            return Matrix4x4.CreateScale(1f / sourceWidth, 1f / sourceHeight, 1) * B3D(S, destination.RightBottom) * S;
+        }
+
+        /// <summary>
+        /// Find Homography.  
+        /// </summary>
         /// <param name="sourceX"> The x of source rectangle. </param>
         /// <param name="sourceY"> The y of source rectangle. </param>
         /// <param name="sourceWidth"> The width of source rectangle. </param>
@@ -49,6 +74,29 @@ namespace FanKit.Transformers
             M31 = -sourceX / sourceWidth,
             M32 = -sourceY / sourceHeight,
         } * FindHomographyFromIdentity(destination);
+
+        /// <summary>
+        /// Find Homography.  
+        /// </summary>
+        /// <param name="sourceX"> The x of source rectangle. </param>
+        /// <param name="sourceY"> The y of source rectangle. </param>
+        /// <param name="sourceWidth"> The width of source rectangle. </param>
+        /// <param name="sourceHeight"> The height of source rectangle. </param>
+        /// <param name="destination"> The destination Transformer. </param>
+        /// <returns> The homologous matrix. </returns>
+        public static Matrix4x4 FindHomography3D(float sourceX, float sourceY, float sourceWidth, float sourceHeight, ITransformerLTRB destination)
+        {
+            Matrix4x4 S = FindHomographyFromIdentity3D(destination);
+            return new Matrix4x4
+            {
+                M11 = 1 / sourceWidth,
+                M22 = 1 / sourceHeight,
+                M33 = 1,
+                M41 = -sourceX / sourceWidth,
+                M42 = -sourceY / sourceHeight,
+                M44 = 1,
+            } * B3D(S, destination.RightBottom) * S;
+        }
 
         /// <summary>
         /// Find Homography.  
@@ -178,6 +226,41 @@ namespace FanKit.Transformers
         {
             Matrix3x2 m = FindHomographyFromIdentity(source);
             return Matrix3x2.Invert(m, out m) ? m * FindHomographyFromIdentity(destination) : FindHomographyFromIdentity(destination);
+        }
+
+        /// <summary>
+        /// Find Homography.  
+        /// </summary>
+        /// <param name="source"> The source Transformer. </param>
+        /// <param name="destination"> The destination Transformer. </param>
+        /// <returns> The homologous matrix. </returns>
+        public static Matrix4x4 FindHomography3D(TransformerRect source, ITransformerLTRB destination)
+            => FindHomography3D(source.Left, source.Top, source.Width, source.Height, destination);
+
+        private static Matrix4x4 B3D(Matrix4x4 A, Vector2 lrt)
+        {
+            //  A Matrix -> a b
+            float den = A.M11 * A.M22 - A.M12 * A.M21;
+            float a = (A.M22 * lrt.X - A.M21 * lrt.Y + A.M21 * A.M42 - A.M22 * A.M41) / den;
+            float b = (A.M11 * lrt.Y - A.M12 * lrt.X + A.M12 * A.M41 - A.M11 * A.M42) / den;
+
+            // compute B Matrix
+            // (0, 0)->(0, 0)
+            // (0, 1)->(0, 1)
+            // (1, 0)->(1, 0)
+            // (1, 1)->(a, b)
+            float ab1 = a + b - 1;
+            float scaleX = a / ab1;
+            float scaleY = b / ab1;
+            return new Matrix4x4
+            {
+                M11 = scaleX,
+                M14 = scaleX - 1,
+                M22 = scaleY,
+                M24 = scaleY - 1,
+                M33 = 1,
+                M44 = 1,
+            };
         }
     }
 }
