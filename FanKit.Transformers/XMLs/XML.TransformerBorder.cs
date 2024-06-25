f6ab16b51b6c5637702f017707e4c40cb3920717
ysdy44 <ysdy44@outlook.com>
2024-06-25 09:02:40 +0800

    Add XML.TransformerBorder

---
+++ FanKit.Transformers/XMLs/XML.TransformerBorder.cs
@@ -0,0 +1,31 @@
+﻿using System.Xml.Linq;
+
+namespace FanKit.Transformers
+{
+    partial class XML
+    {
+
+        public static XElement SaveTransformerBorder(string elementName, TransformerBorder border)
+        {
+            return new XElement
+            (
+                elementName,
+                new XAttribute("Left", border.Left),
+                new XAttribute("Top", border.Top),
+                new XAttribute("Right", border.Right),
+                new XAttribute("Bottom", border.Bottom)
+            );
+        }
+
+        public static TransformerBorder LoadTransformerBorder(XElement element)
+        {
+            TransformerBorder result = new TransformerBorder();
+            if (element.Attribute("Left") is XAttribute left) result.Left = (float)left;
+            if (element.Attribute("Top") is XAttribute top) result.Top = (float)top;
+            if (element.Attribute("Right") is XAttribute right) result.Right = (float)right;
+            if (element.Attribute("Bottom") is XAttribute bottom) result.Bottom = (float)bottom;
+            return result;
+        }
+
+    }
+}
