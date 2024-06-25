e21864ca885e99d03cd978d7d111a9e55777977e
ysdy44 <ysdy44@outlook.com>
2024-06-25 19:48:10 +0800

    L10n: Add 45

--- FanKit.Transformers/Extensions/CanvasDrawingSessionExtensions.Node.cs
+++ FanKit.Transformers/Extensions/CanvasDrawingSessionExtensions.Node.cs
@@ -12,6 +12,12 @@ namespace FanKit.Transformers
         /// <param name="point"> The point. </param>
         public static void DrawNode(this CanvasDrawingSession drawingSession, Vector2 point) => CanvasDrawingSessionExtensions.DrawNode1Core(drawingSession, point.X, point.Y, Windows.UI.Colors.DodgerBlue);
 
+        /// <summary>
+        /// Draw a ⊙.
+        /// </summary>
+        /// <param name="drawingSession"> The drawing-session. </param>
+        /// <param name="x"> The X-axis value of the point. </param>
+        /// <param name="y"> The Y-axis position of the point. </param>
         public static void DrawNode(this CanvasDrawingSession drawingSession, float x, float y) => CanvasDrawingSessionExtensions.DrawNode1Core(drawingSession, x, y, Windows.UI.Colors.DodgerBlue);
 
         /// <summary>
@@ -22,6 +28,12 @@ namespace FanKit.Transformers
         /// <param name="accentColor"> The accent color. </param>
         public static void DrawNode(this CanvasDrawingSession drawingSession, Vector2 point, Windows.UI.Color accentColor) => CanvasDrawingSessionExtensions.DrawNode1Core(drawingSession, point.X, point.Y, accentColor);
 
+        /// <summary>
+        /// Draw a ⊙.
+        /// </summary>
+        /// <param name="drawingSession"> The drawing-session. </param>
+        /// <param name="x"> The X-axis value of the point. </param>
+        /// <param name="y"> The Y-axis position of the point. </param>
         public static void DrawNode(this CanvasDrawingSession drawingSession, float x, float y, Windows.UI.Color accentColor) => CanvasDrawingSessionExtensions.DrawNode1Core(drawingSession, x, y, accentColor);
 
         private static void DrawNode1Core(CanvasDrawingSession drawingSession, float x, float y, Windows.UI.Color accentColor)
@@ -39,6 +51,12 @@ namespace FanKit.Transformers
         /// <param name="point"> The point. </param>
         public static void DrawNode2(this CanvasDrawingSession drawingSession, Vector2 point) => CanvasDrawingSessionExtensions.DrawNode2Core(drawingSession, point.X, point.Y, Windows.UI.Colors.DodgerBlue);
 
+        /// <summary>
+        /// Draw a ◉.
+        /// </summary>
+        /// <param name="drawingSession"> The drawing-session. </param>
+        /// <param name="x"> The X-axis value of the point. </param>
+        /// <param name="y"> The Y-axis position of the point. </param>
         public static void DrawNode2(this CanvasDrawingSession drawingSession, float x, float y) => CanvasDrawingSessionExtensions.DrawNode2Core(drawingSession, x, y, Windows.UI.Colors.DodgerBlue);
 
         /// <summary>
@@ -49,6 +67,13 @@ namespace FanKit.Transformers
         /// <param name="accentColor"> The accent color. </param>
         public static void DrawNode2(this CanvasDrawingSession drawingSession, Vector2 point, Windows.UI.Color accentColor) => CanvasDrawingSessionExtensions.DrawNode2Core(drawingSession, point.X, point.Y, accentColor);
 
+        /// <summary>
+        /// Draw a ◉.
+        /// </summary>
+        /// <param name="drawingSession"> The drawing-session. </param>
+        /// <param name="x"> The X-axis value of the point. </param>
+        /// <param name="y"> The Y-axis position of the point. </param>
+        /// <param name="accentColor"> The accent color. </param>
         public static void DrawNode2(this CanvasDrawingSession drawingSession, float x, float y, Windows.UI.Color accentColor) => CanvasDrawingSessionExtensions.DrawNode2Core(drawingSession, x, y, accentColor);
 
         private static void DrawNode2Core(CanvasDrawingSession drawingSession, float x, float y, Windows.UI.Color accentColor)
@@ -66,6 +91,12 @@ namespace FanKit.Transformers
         /// <param name="point"> The point. </param>
         public static void DrawNode3(this CanvasDrawingSession drawingSession, Vector2 point) => CanvasDrawingSessionExtensions.DrawNode3Core(drawingSession, point.X, point.Y, Windows.UI.Colors.DodgerBlue);
 
+        /// <summary>
+        /// Draw a ◻.
+        /// </summary>
+        /// <param name="drawingSession"> The drawing-session. </param>
+        /// <param name="x"> The X-axis value of the point. </param>
+        /// <param name="y"> The Y-axis position of the point. </param>
         public static void DrawNode3(this CanvasDrawingSession drawingSession, float x, float y) => CanvasDrawingSessionExtensions.DrawNode3Core(drawingSession, x, y, Windows.UI.Colors.DodgerBlue);
 
         /// <summary>
@@ -76,6 +107,13 @@ namespace FanKit.Transformers
         /// <param name="accentColor"> The accent color. </param>
         public static void DrawNode3(this CanvasDrawingSession drawingSession, Vector2 point, Windows.UI.Color accentColor) => CanvasDrawingSessionExtensions.DrawNode3Core(drawingSession, point.X, point.Y, accentColor);
 
+        /// <summary>
+        /// Draw a ◻.
+        /// </summary>
+        /// <param name="drawingSession"> The drawing-session. </param>
+        /// <param name="x"> The X-axis value of the point. </param>
+        /// <param name="y"> The Y-axis position of the point. </param>
+        /// <param name="accentColor"> The accent color. </param>
         public static void DrawNode3(this CanvasDrawingSession drawingSession, float x, float y, Windows.UI.Color accentColor) => CanvasDrawingSessionExtensions.DrawNode3Core(drawingSession, x, y, accentColor);
 
         private static void DrawNode3Core(CanvasDrawingSession drawingSession, float x, float y, Windows.UI.Color accentColor)
@@ -93,6 +131,12 @@ namespace FanKit.Transformers
         /// <param name="point"> The point. </param>
         public static void DrawNode4(this CanvasDrawingSession drawingSession, Vector2 point) => CanvasDrawingSessionExtensions.DrawNode4Core(drawingSession, point.X, point.Y, Windows.UI.Colors.DodgerBlue);
 
+        /// <summary>
+        /// Draw a ◼.
+        /// </summary>
+        /// <param name="drawingSession"> The drawing-session. </param>
+        /// <param name="x"> The X-axis value of the point. </param>
+        /// <param name="y"> The Y-axis position of the point. </param>
         public static void DrawNode4(this CanvasDrawingSession drawingSession, float x, float y) => CanvasDrawingSessionExtensions.DrawNode4Core(drawingSession, x, y, Windows.UI.Colors.DodgerBlue);
 
         /// <summary>
@@ -103,6 +147,13 @@ namespace FanKit.Transformers
         /// <param name="accentColor"> The accent color. </param>
         public static void DrawNode4(this CanvasDrawingSession drawingSession, Vector2 point, Windows.UI.Color accentColor) => CanvasDrawingSessionExtensions.DrawNode4Core(drawingSession, point.X, point.Y, accentColor);
 
+        /// <summary>
+        /// Draw a ◼.
+        /// </summary>
+        /// <param name="drawingSession"> The drawing-session. </param>
+        /// <param name="x"> The X-axis value of the point. </param>
+        /// <param name="y"> The Y-axis position of the point. </param>
+        /// <param name="accentColor"> The accent color. </param>
         public static void DrawNode4(this CanvasDrawingSession drawingSession, float x, float y, Windows.UI.Color accentColor) => CanvasDrawingSessionExtensions.DrawNode4Core(drawingSession, x, y, accentColor);
 
         private static void DrawNode4Core(CanvasDrawingSession drawingSession, float x, float y, Windows.UI.Color accentColor)
@@ -120,6 +171,12 @@ namespace FanKit.Transformers
         /// <param name="point"> The point. </param>
         public static void DrawNode5(this CanvasDrawingSession drawingSession, Vector2 point) => CanvasDrawingSessionExtensions.DrawNode5Core(drawingSession, point.X, point.Y, Windows.UI.Colors.DodgerBlue);
 
+        /// <summary>
+        /// Draw a ◌.
+        /// </summary>
+        /// <param name="drawingSession"> The drawing-session. </param>
+        /// <param name="x"> The X-axis value of the point. </param>
+        /// <param name="y"> The Y-axis position of the point. </param>
         public static void DrawNode5(this CanvasDrawingSession drawingSession, float x, float y) => CanvasDrawingSessionExtensions.DrawNode5Core(drawingSession, x, y, Windows.UI.Colors.DodgerBlue);
 
         /// <summary>
@@ -130,6 +187,13 @@ namespace FanKit.Transformers
         /// <param name="accentColor"> The accent color. </param>
         public static void DrawNode5(this CanvasDrawingSession drawingSession, Vector2 point, Windows.UI.Color accentColor) => CanvasDrawingSessionExtensions.DrawNode5Core(drawingSession, point.X, point.Y, accentColor);
 
+        /// <summary>
+        /// Draw a ◌.
+        /// </summary>
+        /// <param name="drawingSession"> The drawing-session. </param>
+        /// <param name="x"> The X-axis value of the point. </param>
+        /// <param name="y"> The Y-axis position of the point. </param>
+        /// <param name="accentColor"> The accent color. </param>
         public static void DrawNode5(this CanvasDrawingSession drawingSession, float x, float y, Windows.UI.Color accentColor) => CanvasDrawingSessionExtensions.DrawNode5Core(drawingSession, x, y, accentColor);
 
         private static void DrawNode5Core(CanvasDrawingSession drawingSession, float x, float y, Windows.UI.Color accentColor)
