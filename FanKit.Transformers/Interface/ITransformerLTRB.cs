341a117623bcbb5a88cdc027d56607c8009a2ec4
ysdy44 <ysdy44@outlook.com>
2024-07-01 01:39:13 +0800

    L10n: Subtract 10

--- FanKit.Transformers/Interface/ITransformerLTRB.cs
+++ FanKit.Transformers/Interface/ITransformerLTRB.cs
@@ -2,30 +2,18 @@
 
 namespace FanKit.Transformers
 {
-    /// <summary>
-    /// A interface provides four positions (top-left, top-right, bottom-right, bottom-left corners).
-    /// </summary>
     public interface ITransformerLTRB
     {
-        /// <summary>  Gets the X-axis value of the center. </summary>
         float CenterX { get; }
-        /// <summary>  Gets the Y-axis position of the center. </summary>
         float CenterY { get; }
-        /// <summary> Gets the position of the center. </summary>
         Vector2 Center { get; }
 
-        /// <summary> Gets the position of the center of bottom-left and top-left corners. </summary>
         Vector2 CenterLeft { get; }
-        /// <summary> Gets the position of the center of top-left and top-right corners. </summary>
         Vector2 CenterTop { get; }
-        /// <summary> Gets the position of the center of top-right and bottom-right corners. </summary>
         Vector2 CenterRight { get; }
-        /// <summary> Gets the position of the center of bottom-right and bottom-left corners. </summary>
         Vector2 CenterBottom { get; }
 
-        /// <summary> Gets the horizontal vector. </summary>
         Vector2 Horizontal { get; }
-        /// <summary> Gets the vertical vector. </summary>
         Vector2 Vertical { get; }
     }
 }
