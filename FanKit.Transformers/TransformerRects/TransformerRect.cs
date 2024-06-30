4e03341563440595b26a14deda2cf3709fa151e2
ysdy44 <ysdy44@outlook.com>
2024-07-01 02:00:40 +0800

    Folder Remove: TransformerRects

--- FanKit.Transformers/TransformerRects/TransformerRect.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.Graphics.Canvas;
-using Microsoft.Graphics.Canvas.Geometry;
-using System.Numerics;
-using Windows.Foundation;
-
-namespace FanKit.Transformers
-{
-    public partial struct TransformerRect { }
-}
